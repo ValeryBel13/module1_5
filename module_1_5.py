3451fe4c0918a6aad551bb3d2219094782817462
ValeryBel13 <Cerkov94@gmail.com>
2024-12-08 02:02:30 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+#immutable_var_tuple_=(1, 2 , " a ", " b" )
+#print(immutable_var_tuple_ )
+#immutable_var_tuple_ [0] = 200
+#print(immutable_var_tuple_)
+#кортеж не поддерживает обращение по элементам,его нельзя редактировать или как-либо изменять
+mutable_list=['dog' , 'cat' , 'ping' , 'book']
+print(mutable_list)
+mutable_list [0]='apple'
+mutable_list [2] ='phone'
+print(mutable_list)
